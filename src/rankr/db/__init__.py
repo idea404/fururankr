b1bdac48dc312628a6adcb0647d0d2bd063927cc
Dennis <10233439+idea404@users.noreply.github.com>
2022-02-01 21:24:12 +0000

    session cx manager exception

--- src/rankr/db/__init__.py
+++ src/rankr/db/__init__.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 from contextlib import contextmanager
 
+import structlog
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, scoped_session
 from sqlalchemy.orm import sessionmaker
@@ -9,6 +10,9 @@ from sqlalchemy.pool import QueuePool
 
 
 DB_PATH = pathlib.Path(os.getcwd()).joinpath("rankr").joinpath("db")
+
+
+logger = structlog.get_logger()
 
 
 def create_db_session_from_cfg(echo: bool = False) -> Session:
@@ -55,15 +59,15 @@ def create_db_scoped_session(echo: bool = False) -> scoped_session:
 @contextmanager
 def scoped_session_context_manager(scoped_session_class: scoped_session):
     """Provide a transactional scope around a series of operations."""
-    class SQLRollBackException(Exception):
-        pass
-
     session = scoped_session_class()
     try:
         yield session
         session.commit()
     except:
         session.rollback()
-        raise SQLRollBackException("Rolling back changes as except clause triggered")
+        logger.exception(
+            "Failed session within context manager (not caught by any handlers)"
+        )
+        raise
     finally:
         session.close()
